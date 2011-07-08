625f924d8648812d1f0a16fb94287e09f0d57f1b
Austin Bischoff <austin.bischoff@gmail.com>
2011-07-07 21:31:25 -0500

    Added more base games for existing protocols.

---
+++ gameq/protocols/graw2.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of GameQ.
+ *
+ * GameQ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GameQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Ghost Recon: Advanced Warfighter 2 Protocol Class
+ *
+ * @author Austin Bischoff <austin@codebeard.com>
+ */
+class GameQ_Protocols_Graw2 extends GameQ_Protocols_Gamespy2
+{
+	protected $name = "graw2";
+	protected $name_long = "Ghost Recon: Advanced Warfighter 2";
+
+	protected $port = 16250;
+}
