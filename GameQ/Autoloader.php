e4971419f3838b3c744f39587590118634403d20
Austin Bischoff <austin.bischoff@gmail.com>
2015-01-25 20:40:35 -0600

    Updated Autoloader.php to fix bug in spl that does not use proper case for the file name.  Also fixed path reference for set_include_path().  Ref #200

--- GameQ/Autoloader.php
+++ GameQ/Autoloader.php
@@ -6,10 +6,14 @@
  *
  * i.e. require_once('/path/to/GameQ/Autoloader.php');
  */
-set_include_path(get_include_path() . PATH_SEPARATOR .  realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
+set_include_path(get_include_path() . PATH_SEPARATOR . realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '..'
+                 . DIRECTORY_SEPARATOR);
 
 // We include all .php files
 spl_autoload_extensions(".php");
 
-// Register the autoloader
-spl_autoload_register();
+// Register the autoloader but we overload the function to allow casing to work as expected.
+spl_autoload_register(function ($class) {
+
+    include $class . '.php';
+});
