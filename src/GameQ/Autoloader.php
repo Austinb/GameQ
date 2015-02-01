8cfa7f082abee16a010ce337dd35896c36bcfaf1
Austin Bischoff <austin.bischoff@gmail.com>
2015-01-31 18:18:27 -0600

    Added missing license and fixed comments.

--- src/GameQ/Autoloader.php
+++ src/GameQ/Autoloader.php
@@ -1,14 +1,30 @@
 <?php
+/**
+ * This file is part of GameQ.
+ *
+ * GameQ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GameQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
 /*
- * Simple auto loader to allow GameQ to function the same as if it were loaded via Composer
+ * A simple PSR-4 spec auto loader to allow GameQ to function the same as if it were loaded via Composer
  *
  * To use this just include this file in your script and the GameQ namespace will be made available
  *
  * i.e. require_once('/path/to/src/GameQ/Autoloader.php');
- */
-
-/**
- * A PSR-4 autoloader for non-composer installs.
+ *
  * See: https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md
  */
 spl_autoload_register(function ($class) {
