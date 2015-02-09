ea05cd009626dda77216b08fdc22f4950ccdd440
Austin Bischoff <austin.bischoff@gmail.com>
2015-02-08 19:17:32 -0600

    Updated armedassault2 (arma2) to use source.  Check your query port as the default query port is 27016 for these servers now. Fixes #202

--- gameq/protocols/armedassault2.php
+++ gameq/protocols/armedassault2.php
@@ -21,24 +21,13 @@
  *
  * @author Austin Bischoff <austin@codebeard.com>
  */
-class GameQ_Protocols_Armedassault2 extends GameQ_Protocols_Gamespy3
+class GameQ_Protocols_Armedassault2 extends GameQ_Protocols_Source
 {
-	protected $name = "armedassault2";
+
+	protected $name      = "armedassault2";
+
 	protected $name_long = "Armed Assault 2";
 
-	protected $port = 2302;
+	protected $port      = 2302;
 
-	protected function parsePlayerTeamInfoNew(GameQ_Buffer &$buf, GameQ_Result &$result)
-	{
-    	// Read the buffer and replace the team_ sub-section under the players section becasue it is broke
-    	$buf_fixed = preg_replace('/team_(.*)score_/m', 'score_', $buf->getBuffer());
-
-    	// Replace the buffer with the "fixed" buffer
-    	$buf = new GameQ_Buffer($buf_fixed);
-
-    	unset($buf_fixed);
-
-    	// Now we continue on with the parent
-    	return parent::parsePlayerTeamInfo($buf, $result);
-	}
 }
