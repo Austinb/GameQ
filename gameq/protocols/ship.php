3745ebcd4a640546e628c3224f66fcc0e436188b
Nikolay <Rostov114@gmail.com>
2015-02-17 16:11:40 +0300

    Add support The Ship game

---
+++ gameq/protocols/ship.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of GameQ.
+ *
+ * GameQ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GameQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * The Ship Protocol Class
+ *
+ * @author Nikolay Ipanyuk <rostov114@gmail.com>
+ */
+class GameQ_Protocols_Ship extends GameQ_Protocols_Source
+{
+	protected $name = "ship";
+	protected $name_long = "The Ship";
+}
