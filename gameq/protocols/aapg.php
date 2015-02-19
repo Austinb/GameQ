96094dda9782623f523e3b33d343154ac9b1c32a
Austin Bischoff <austin.bischoff@gmail.com>
2015-02-19 15:35:27 -0600

    Added AA Proving Grounds.  Fixes #201

---
+++ gameq/protocols/aapg.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of GameQ.
+ *
+ * GameQ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GameQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * America's Army Proving Grounds
+ *
+ * @author Austin Bischoff <austin@codebeard.com>
+ */
+class GameQ_Protocols_Aapg extends GameQ_Protocols_Aa3
+{
+	protected $name = "aapg";
+	protected $name_long = " America's Army: Proving Grounds";
+}
