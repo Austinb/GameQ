9c15a7ccb03c06c8c626e870a3f7ee779893bda5
Austin Bischoff <austin.bischoff@gmail.com>
2015-02-19 16:20:26 -0600

    Adder America's Army 3 (version 3.2+)

---
+++ src/GameQ/Protocols/Aa3.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * This file is part of GameQ.
+ *
+ * GameQ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GameQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace GameQ\Protocols;
+
+/**
+ * Class Aa3
+ *
+ * @package GameQ\Protocols
+ * @author  Austin Bischoff <austin@codebeard.com>
+ */
+class Aa3 extends Source
+{
+
+    /**
+     * String name of this protocol class
+     *
+     * @type string
+     */
+    protected $name = 'aa3';
+
+    /**
+     * Longer string name of this protocol class
+     *
+     * @type string
+     */
+    protected $name_long = "America's Army 3";
+
+    /**
+     * Query port = client_port + 18243
+     *
+     * client_port default 8777
+     * query_port default 27020
+     *
+     * @type int
+     */
+    protected $port_diff = 18243;
+}
