dbef43f73a107d8545aa8c1d94be7d838bd11935
Austin Bischoff <austin.bischoff@gmail.com>
2015-02-23 22:53:52 -0600

    Added minecraft protocol (gamespy3).

---
+++ src/GameQ/Protocols/Minecraft.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * This file is part of GameQ.
+ *
+ * GameQ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GameQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace GameQ\Protocols;
+
+/**
+ * Minecraft Protocol Class
+ *
+ * Thanks to https://github.com/xPaw/PHP-Minecraft-Query for helping me realize this is
+ * Gamespy 3 Protocol.  Make sure you enable the items below for it to work.
+ *
+ * Information from original author:
+ * Instructions
+ *
+ * Before using this class, you need to make sure that your server is running GS4 status listener.
+ *
+ * Look for those settings in server.properties:
+ *
+ *    enable-query=true
+ *    query.port=25565
+ *
+ * @package GameQ\Protocols
+ *
+ * @author  Austin Bischoff <austin@codebeard.com>
+ */
+class Minecraft extends Gamespy3
+{
+
+    /**
+     * String name of this protocol class
+     *
+     * @type string
+     */
+    protected $name = 'minecraft';
+
+    /**
+     * Longer string name of this protocol class
+     *
+     * @type string
+     */
+    protected $name_long = "Minecraft";
+
+    /**
+     * The client join link
+     *
+     * @type string
+     */
+    protected $join_link = "minecraft://%s:%d/";
+
+    /**
+     * Normalize settings for this protocol
+     *
+     * @type array
+     */
+    protected $normalize = [
+        // General
+        'general' => [
+            // target       => source
+            'dedicated'  => 'dedicated',
+            'gametype'   => 'game_id',
+            'hostname'   => 'hostname',
+            'mapname'    => 'map',
+            'maxplayers' => 'maxplayers',
+            'numplayers' => 'numplayers',
+            'password'   => 'password',
+        ],
+        // Individual
+        'player'  => [
+            'name' => 'player',
+        ],
+    ];
+
+    /**
+     * Intercept the processResponse so we can add some extra fields
+     *
+     * @return array
+     */
+    public function processResponse()
+    {
+
+        $result = parent::processResponse();
+
+        $result['dedicated'] = 1; // All servers are dedicated as far as I can tell
+        $result['password'] = 0; // Unsure if you can password servers, cant read that value
+
+        return $result;
+    }
+}
