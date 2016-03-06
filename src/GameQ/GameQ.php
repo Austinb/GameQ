Austinb/GameQ

commit d4735a9031f3313c5ec6e45dd2bdc9c0b5a0c26a

Added query exception handling.  Since tests for the protocols are missing this was overlooked.  Fixes #293

src/GameQ/GameQ.php
@@ -18,6 +18,7 @@
 namespace GameQ;
 
 use GameQ\Exception\Protocol as ProtocolException;
+use GameQ\Exception\Query as QueryException;
 
 /**
  * Base GameQ Class
@@ -123,7 +124,7 @@ class GameQ
     public function __get($option)
     {
 
-        return isset($this->options[$option]) ? $this->options[$option] : null;
+        return isset($this->options[ $option ]) ? $this->options[ $option ] : null;
     }
 
     /**
@@ -137,7 +138,7 @@ class GameQ
     public function __set($option, $value)
     {
 
-        $this->options[$option] = $value;
+        $this->options[ $option ] = $value;
 
         return true;
     }
@@ -170,7 +171,7 @@ class GameQ
     {
 
         // Add and validate the server
-        $this->servers[uniqid()] = new Server($server_info);
+        $this->servers[ uniqid() ] = new Server($server_info);
 
         return $this; // Make calls chainable
     }
@@ -259,7 +260,7 @@ class GameQ
     {
 
         // Add the filter
-        $this->options['filters'][strtolower($filterName)] = $options;
+        $this->options['filters'][ strtolower($filterName) ] = $options;
 
         return $this;
     }
@@ -279,7 +280,7 @@ class GameQ
 
         // Remove this filter if it has been defined
         if (array_key_exists($filterName, $this->options['filters'])) {
-            unset($this->options['filters'][$filterName]);
+            unset($this->options['filters'][ $filterName ]);
         }
 
         return $this;
@@ -327,7 +328,7 @@ class GameQ
             ksort($result);
 
             // Add the result to the results array
-            $results[$server->id()] = $result;
+            $results[ $server->id() ] = $result;
         }
 
         return $results;
@@ -365,14 +366,22 @@ class GameQ
                     $this->timeout
                 );
 
-                // Now write the challenge packet to the socket.
-                $socket->write($server->protocol()->getPacket(Protocol::PACKET_CHALLENGE));
+                try {
+                    // Now write the challenge packet to the socket.
+                    $socket->write($server->protocol()->getPacket(Protocol::PACKET_CHALLENGE));
 
-                // Add the socket information so we can reference it easily
-                $sockets[(int) $socket->get()] = [
-                    'server_id' => $server_id,
-                    'socket'    => $socket,
-                ];
+                    // Add the socket information so we can reference it easily
+                    $sockets[ (int) $socket->get() ] = [
+                        'server_id' => $server_id,
+                        'socket'    => $socket,
+                    ];
+
+                } catch (QueryException $e) {
+                    // Check to see if we are in debug, if so bubble up the exception
+                    if ($this->debug) {
+                        throw new \Exception($e->getMessage(), $e->getCode(), $e);
+                    }
+                }
 
                 unset($socket);
 
@@ -392,20 +401,20 @@ class GameQ
             // Iterate over the challenge responses
             foreach ($responses as $socket_id => $response) {
                 // Back out the server_id we need to update the challenge response for
-                $server_id = $sockets[$socket_id]['server_id'];
+                $server_id = $sockets[ $socket_id ]['server_id'];
 
                 // Make this into a buffer so it is easier to manipulate
                 $challenge = new Buffer(implode('', $response));
 
                 // Grab the server instance
                 /* @var $server \GameQ\Server */
-                $server = $this->servers[$server_id];
+                $server = $this->servers[ $server_id ];
 
                 // Apply the challenge
                 $server->protocol()->challengeParseAndApply($challenge);
 
                 // Add this socket to be reused, has to be reused in GameSpy3 for example
-                $server->socketAdd($sockets[$socket_id]['socket']);
+                $server->socketAdd($sockets[ $socket_id ]['socket']);
 
                 // Clear
                 unset($server);
@@ -453,8 +462,15 @@ class GameQ
 
             // Iterate over all the packets we need to send
             foreach ($packets as $packet_data) {
-                // Now write the packet to the socket.
-                $socket->write($packet_data);
+                try {
+                    // Now write the packet to the socket.
+                    $socket->write($packet_data);
+                } catch (QueryException $e) {
+                    // Check to see if we are in debug, if so bubble up the exception
+                    if ($this->debug) {
+                        throw new \Exception($e->getMessage(), $e->getCode(), $e);
+                    }
+                }
 
                 // Let's sleep shortly so we are not hammering out calls rapid fire style
                 usleep($this->write_wait);
@@ -463,7 +479,7 @@ class GameQ
             unset($packets);
 
             // Add the socket information so we can reference it easily
-            $sockets[(int) $socket->get()] = [
+            $sockets[ (int) $socket->get() ] = [
                 'server_id' => $server_id,
                 'socket'    => $socket,
             ];
@@ -481,11 +497,11 @@ class GameQ
         // Iterate over the responses
         foreach ($responses as $socket_id => $response) {
             // Back out the server_id
-            $server_id = $sockets[$socket_id]['server_id'];
+            $server_id = $sockets[ $socket_id ]['server_id'];
 
             // Grab the server instance
             /* @var $server \GameQ\Server */
-            $server = $this->servers[$server_id];
+            $server = $this->servers[ $server_id ];
 
             // Save the response from this packet
             $server->protocol()->packetResponse($response);

src/GameQ/Query/Native.php
@@ -100,7 +100,7 @@ class Native extends Core
 
         // Create the socket
         if (($this->socket =
-                stream_socket_client($remote_addr, $errno, $errstr, $this->timeout, STREAM_CLIENT_CONNECT, $context))
+                @stream_socket_client($remote_addr, $errno, $errstr, $this->timeout, STREAM_CLIENT_CONNECT, $context))
             !== false
         ) {
             // Set the read timeout on the streams
@@ -110,7 +110,7 @@ class Native extends Core
             stream_set_blocking($this->socket, $this->blocking);
         } else {
             throw new Exception(
-                __METHOD__ . " Error creating socket to server {$this->ip}:{$this->port}. Error: " . $errstr,
+                __METHOD__ . " - Error creating socket to server {$this->ip}:{$this->port}. Error: " . $errstr,
                 $errno
             );
         }
@@ -143,10 +143,10 @@ class Native extends Core
         foreach ($sockets as $socket_id => $socket_data) {
             // Get the socket
             /* @var $socket \GameQ\Query\Core */
-            $socket = $socket_data['socket'];
+            $socket = $socket_data[ 'socket' ];
 
             // Append the actual socket we are listening to
-            $sockets_tmp[$socket_id] = $socket->get();
+            $sockets_tmp[ $socket_id ] = $socket->get();
 
             unset($socket);
         }
@@ -191,12 +191,12 @@ class Native extends Core
                 // Check to see if the response is empty, if so we are done with this server
                 if (strlen($response) == 0) {
                     // Remove this server from any future read loops
-                    unset($sockets_tmp[(int) $socket]);
+                    unset($sockets_tmp[ (int) $socket ]);
                     continue;
                 }
 
                 // Add the response we got back
-                $responses[(int) $socket][] = $response;
+                $responses[ (int) $socket ][] = $response;
             }
 
             // Because stream_select modifies read we need to reset it each time to the original array of sockets

commit cb3f1789f5251d96d2cf0d6d8a3c7604e8a895a5

Fixed bug introduced in d4735a9 where data was still being sent to a bogus socket.  Was throwing exceptions even with debug mode off.  Please verify in #293.

src/GameQ/GameQ.php
@@ -460,29 +460,31 @@ class GameQ
                 );
             }
 
-            // Iterate over all the packets we need to send
-            foreach ($packets as $packet_data) {
-                try {
+            try {
+                // Iterate over all the packets we need to send
+                foreach ($packets as $packet_data) {
                     // Now write the packet to the socket.
                     $socket->write($packet_data);
-                } catch (QueryException $e) {
-                    // Check to see if we are in debug, if so bubble up the exception
-                    if ($this->debug) {
-                        throw new \Exception($e->getMessage(), $e->getCode(), $e);
-                    }
+
+                    // Let's sleep shortly so we are not hammering out calls rapid fire style
+                    usleep($this->write_wait);
                 }
 
-                // Let's sleep shortly so we are not hammering out calls rapid fire style
-                usleep($this->write_wait);
+                unset($packets);
+
+                // Add the socket information so we can reference it easily
+                $sockets[ (int) $socket->get() ] = [
+                    'server_id' => $server_id,
+                    'socket'    => $socket,
+                ];
+            } catch (QueryException $e) {
+                // Check to see if we are in debug, if so bubble up the exception
+                if ($this->debug) {
+                    throw new \Exception($e->getMessage(), $e->getCode(), $e);
+                }
+
+                break;
             }
-
-            unset($packets);
-
-            // Add the socket information so we can reference it easily
-            $sockets[ (int) $socket->get() ] = [
-                'server_id' => $server_id,
-                'socket'    => $socket,
-            ];
 
             // Clean up the sockets, if any left over
             $server->socketCleanse();

src/GameQ/Query/Native.php
@@ -109,6 +109,10 @@ class Native extends Core
             // Set blocking mode
             stream_set_blocking($this->socket, $this->blocking);
         } else {
+            // Reset socket
+            $this->socket = null;
+
+            // Something bad happened, throw query exception
             throw new Exception(
                 __METHOD__ . " - Error creating socket to server {$this->ip}:{$this->port}. Error: " . $errstr,
                 $errno
@@ -143,7 +147,7 @@ class Native extends Core
         foreach ($sockets as $socket_id => $socket_data) {
             // Get the socket
             /* @var $socket \GameQ\Query\Core */
-            $socket = $socket_data[ 'socket' ];
+            $socket = $socket_data['socket'];
 
             // Append the actual socket we are listening to
             $sockets_tmp[ $socket_id ] = $socket->get();