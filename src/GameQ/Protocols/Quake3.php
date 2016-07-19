dd59d057b794852a6a0b31f9159d0e38258b6fba
Austinb <austin@codebeard.com>
2016-07-18 19:31:26 -0500

    Added empty header check for empty response in quake 3.  Fixes #323

--- src/GameQ/Protocols/Quake3.php
+++ src/GameQ/Protocols/Quake3.php
@@ -104,7 +104,7 @@ class Quake3 extends Protocol
         $header = $buffer->readString("\x0A");
 
         // Figure out which packet response this is
-        if (!array_key_exists($header, $this->responses)) {
+        if (empty($header) || !array_key_exists($header, $this->responses)) {
             throw new Exception(__METHOD__ . " response type '" . bin2hex($header) . "' is not valid");
         }
 
