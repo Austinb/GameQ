378bc5cce6a9ae380809d33b97b5c572307ed7fc
Austinb <austin@codebeard.com>
2016-07-18 19:05:07 -0500

    Added changes to rules support

--- src/GameQ/Protocols/Armedassault3.php
+++ src/GameQ/Protocols/Armedassault3.php
@@ -18,11 +18,16 @@
 
 namespace GameQ\Protocols;
 
+use GameQ\Buffer;
+use GameQ\Result;
+
 /**
  * Class Armed Assault 3
  *
+ * Rules protocol reference: https://community.bistudio.com/wiki/Arma_3_ServerBrowserProtocol2
+ *
  * @package GameQ\Protocols
- * @author Austin Bischoff <austin@codebeard.com>
+ * @author  Austin Bischoff <austin@codebeard.com>
  */
 class Armedassault3 extends Source
 {
@@ -46,4 +51,64 @@ class Armedassault3 extends Source
      * @type int
      */
     protected $port_diff = 1;
+
+    protected function processRules(Buffer $buffer)
+    {
+
+        //var_dump($buffer);
+
+        // Total number of packets, burn it
+        $buffer->readInt16();
+
+        // Will hold the data string
+        $data = '';
+
+        // Loop until we run out of strings
+        while ($buffer->getLength()) {
+            // Burn the delimiters (i.e. \x01\x04\x00)
+            $buffer->readString();
+
+            // Add the data to the string, we are reassembling it
+            $data .= $buffer->readString();
+        }
+
+        // Make a new buffer with the reassembled data
+        $responseBuffer = new Buffer($data);
+
+        var_dump($responseBuffer->getBuffer());
+
+        // Kill the old buffer, should be empty
+        unset($buffer, $data);
+
+        // Set the result to a new result instance
+        $result = new Result();
+
+        /*$result->add('protocol_version', $responseBuffer->readInt8());
+        $result->add('overflow', $responseBuffer->readInt8());
+        $result->add('dlc', $responseBuffer->readInt8());
+        $responseBuffer->skip(); // Burn, reserved
+        $result->add('difficulty', $responseBuffer->readInt8());
+        $result->add('crosshair', $responseBuffer->readInt8());
+
+        $result->add('karts', $responseBuffer->readInt32());
+        $result->add('marksmen', $responseBuffer->readInt32());
+        $result->add('helicopters', $responseBuffer->readInt32());
+        $result->add('expansion', $responseBuffer->readInt32());*/
+
+        //$temp = $responseBuffer->read(10);
+        var_dump($responseBuffer->readInt8());
+        var_dump($responseBuffer->readInt8());
+        var_dump($responseBuffer->readInt8());
+        var_dump($responseBuffer->readInt8());
+
+        var_dump($responseBuffer->readInt8());
+        var_dump($responseBuffer->readInt8());
+
+        var_dump($responseBuffer->readInt32());
+
+        var_dump($responseBuffer->getBuffer());
+
+        var_dump($result->fetch());
+        exit;
+    }
 }
