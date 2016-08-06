747fa8a81d2a65e4e13ccd88c984a9f70e94f1a2
Austinb <austin@codebeard.com>
2016-08-06 17:49:56 -0500

    First "working" version of arma3 new rules.  Missing definitions for difficulty levels but all available information has been pulled in. See #299

--- src/GameQ/Protocols/Armedassault3.php
+++ src/GameQ/Protocols/Armedassault3.php
@@ -28,9 +28,31 @@ use GameQ\Result;
  *
  * @package GameQ\Protocols
  * @author  Austin Bischoff <austin@codebeard.com>
+ * @author  Memphis017 <https://github.com/Memphis017>
  */
 class Armedassault3 extends Source
 {
+    /**
+     * DLC ^2 constants
+     */
+    const DLC_KARTS = 1,
+        DLC_MARKSMEN = 2,
+        DLC_HELICOPTER = 4,
+        DLC_APEX = 16;
+
+    /**
+     * Defines the names for the specific game DLCs
+     *
+     * @var array
+     */
+    protected $dlcNames = [
+        self::DLC_KARTS      => 'Karts',
+        self::DLC_MARKSMEN   => 'Marksman',
+        self::DLC_HELICOPTER => 'Helicopter',
+        self::DLC_APEX       => 'Apex',
+    ];
+
+
     /**
      * String name of this protocol class
      *
@@ -52,11 +74,16 @@ class Armedassault3 extends Source
      */
     protected $port_diff = 1;
 
+    /**
+     * Process the rules since Arma3 changed their response for rules
+     *
+     * @param Buffer $buffer
+     *
+     * @return array
+     * @throws \GameQ\Exception\Protocol
+     */
     protected function processRules(Buffer $buffer)
     {
-
-        //var_dump($buffer);
-
         // Total number of packets, burn it
         $buffer->readInt16();
 
@@ -73,12 +100,10 @@ class Armedassault3 extends Source
         }
 
         // Restore escaped sequences
-        $data = str_replace(array("\x01\x01","\x01\x02","\x01\x03"), array("\x01","\x00","\xFF"), $data);
+        $data = str_replace(["\x01\x01", "\x01\x02", "\x01\x03"], ["\x01", "\x00", "\xFF"], $data);
 
         // Make a new buffer with the reassembled data
         $responseBuffer = new Buffer($data);
-
-        var_dump($responseBuffer->getBuffer());
 
         // Kill the old buffer, should be empty
         unset($buffer, $data);
@@ -86,32 +111,73 @@ class Armedassault3 extends Source
         // Set the result to a new result instance
         $result = new Result();
 
-        /*$result->add('protocol_version', $responseBuffer->readInt8());
+        // Get results
+        $result->add('rules_protocol_version', $responseBuffer->readInt8());
         $result->add('overflow', $responseBuffer->readInt8());
-        $result->add('dlc', $responseBuffer->readInt8());
-        $responseBuffer->skip(); // Burn, reserved
-        $result->add('difficulty', $responseBuffer->readInt8());
+        $dlcBit = $responseBuffer->readInt8(); // Grab DLC bit and use it later
+        $responseBuffer->skip(); // Reserved, burn it
+        // Grab difficulty so we can man handle it...
+        $difficulty = $responseBuffer->readInt8();
+
+        // Process difficulty
+        $result->add('3rd_person', $difficulty >> 7);
+        $result->add('advanced_flight_mode', ($difficulty >> 6) & 1);
+        $result->add('difficulty_ai', ($difficulty >> 3) & 3);
+        $result->add('difficulty_level', $difficulty & 3);
+
+        /*var_dump($difficulty);
+        var_dump($difficulty >> 7);
+        var_dump(($difficulty >> 6) & 1);
+        var_dump(($difficulty >> 3) & 3);
+        var_dump($difficulty & 3);*/
+
+        unset($difficulty);
+
+        // Crosshair
         $result->add('crosshair', $responseBuffer->readInt8());
 
-        $result->add('karts', $responseBuffer->readInt32());
-        $result->add('marksmen', $responseBuffer->readInt32());
-        $result->add('helicopters', $responseBuffer->readInt32());
-        $result->add('expansion', $responseBuffer->readInt32());*/
+        // Next are the dlc bits so loop over the dlcBit so we can determine which DLC's are running
+        for ($x = 1; $x <= $dlcBit; $x *= 2) {
+            // Enabled, add it to the list
+            if ($x & $dlcBit) {
+                $result->addSub('dlcs', 'name', $this->dlcNames[$x]);
+                $result->addSub('dlcs', 'hash', dechex($responseBuffer->readInt32()));
+            }
+        }
 
-        //$temp = $responseBuffer->read(10);
-        var_dump($responseBuffer->readInt8());
-        var_dump($responseBuffer->readInt8());
-        var_dump($responseBuffer->readInt8());
-        var_dump($responseBuffer->readInt8());
+        // No longer needed
+        unset($dlcBit);
 
-        var_dump($responseBuffer->readInt8());
-        var_dump($responseBuffer->readInt8());
+        // Grab the mod count
+        $modCount = $responseBuffer->readInt8();
 
-        var_dump($responseBuffer->readInt32());
+        // Loop the mod count and add them
+        for ($x = 0; $x < $modCount; $x++) {
+            // Add the mod to the list
+            $result->addSub('mods', 'hash', dechex($responseBuffer->readInt32()));
+            $result->addSub('mods', 'steam_id', hexdec($responseBuffer->readPascalString(0, true)));
+            $result->addSub('mods', 'name', $responseBuffer->readPascalString(0, true));
+        }
 
-        var_dump($responseBuffer->getBuffer());
+        unset($modCount, $x);
 
-        var_dump($result->fetch());
-        exit;
+        // Burn the signatures count, we will just loop until we run out of strings
+        $responseBuffer->read();
+
+        // Make signatures array
+        $signatures = [];
+
+        // Loop until we run out of strings
+        while ($responseBuffer->getLength()) {
+            //$result->addSub('signatures', 0, $responseBuffer->readPascalString(0, true));
+            $signatures[] = $responseBuffer->readPascalString(0, true);
+        }
+
+        // Add as a simple array
+        $result->add('signatures', $signatures);
+
+        unset($responseBuffer, $signatures);
+
+        return $result->fetch();
     }
 }
