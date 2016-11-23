a4b88330e1bc637f5aecb311b7ca78a8ca7d601a
Austinb <austin@codebeard.com>
2016-11-23 10:18:14 -0600

    Added 7 Days to Die protocol. Fixes #338

---
+++ src/GameQ/Protocols/Sevendaystodie.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * This file is part of GameQ.
+ *
+ * GameQ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GameQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace GameQ\Protocols;
+
+/**
+ * Class 7 Days to Die
+ *
+ * @package GameQ\Protocols
+ * @author Austin Bischoff <austin@codebeard.com>
+ */
+class Sevendaystodie extends Source
+{
+    /**
+     * String name of this protocol class
+     *
+     * @type string
+     */
+    protected $name = 'sevendaystodie';
+
+    /**
+     * Longer string name of this protocol class
+     *
+     * @type string
+     */
+    protected $name_long = "7 Days to Die";
+
+    /**
+     * query_port = client_port + 1
+     *
+     * @type int
+     */
+    protected $port_diff = 1;
+}
