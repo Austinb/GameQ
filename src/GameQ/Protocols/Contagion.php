9a8aafeee0d11b169c1a1a48a677fe0f7c6751d0
Austin Bischoff <Austinb@users.noreply.github.com>
2019-06-25 13:37:33 -0500

    Line spacing

--- src/GameQ/Protocols/Contagion.php
+++ src/GameQ/Protocols/Contagion.php
@@ -15,8 +15,10 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 namespace GameQ\Protocols;
- /**
+
+/**
  * Class Contagion
  *
  * Pre-cursor to the A2S (source) protocol system
