178f1421c73ca80084c02f8c2c3fc0f3c28b5ab7
Wilson <3997415+uilson@users.noreply.github.com>
2019-10-18 01:10:08 +0100

    Added support for a few games (Retry). (#494)
    * Added Support for Brink.
    
    * Added Support for Call of Duty.
    
    * Added Support for Call of Duty: United Offensive.
    
    * Added Support for Crysis.
    
    * Added Support for Crysis 2.
    
    * Added Support for Wolfenstein Enemy Territory.
    
    * Update Crysis.php
    
    Crysis 2 has no test because couldnt find any server online.
    
    * Added Swat4 Protocol and tests
    
    Added Swat4 protocol (extends gamespy2) and its tests.
    
    * Changed protocol of crysis to gamespy 3 again.
    
    * Added Arma Protocol and tests
    
    Added Protocol for Arma (ArmA Armed Assault) and tests, this also expands the Gamespy2 protocol.
    
    * Re-done tests for Coduo
    
    Re-done the tests for coduo (extends quake3) and added 2 more tests, dont see any issue with the players names.
    
    * Re-done tests, with the Crysis protocol instead of gamespy3

---
+++ src/GameQ/Protocols/Swat4.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This file is part of GameQ.
+ *
+ * GameQ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GameQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace GameQ\Protocols;
+
+/**
+ * Class Swat4
+ *
+ * @package GameQ\Protocols
+ *
+ * @author  Wilson Jesus <>
+ */
+class Swat4 extends Gamespy2
+{
+    /**
+     * String name of this protocol class
+     *
+     * @type string
+     */
+    protected $name = 'swat4';
+
+    /**
+     * Longer string name of this protocol class
+     *
+     * @type string
+     */
+    protected $name_long = "SWAT 4";
+
+    /**
+     * query_port = client_port + 1
+     *
+     * @type int
+     */
+    protected $port_diff = 1;
+}
