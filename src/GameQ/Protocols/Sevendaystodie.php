cc4a4ccda10797fa50a990c1f1f00cc94dc163e1
Patrick Henninger <privat@skyraptor.eu>
2022-05-23 16:59:30 +0200

    Fix last commit as tested locally
    Signed-off-by: Patrick Henninger <privat@skyraptor.eu>

--- src/GameQ/Protocols/Sevendaystodie.php
+++ src/GameQ/Protocols/Sevendaystodie.php
@@ -41,11 +41,9 @@ class Sevendaystodie extends Source
     protected $name_long = "7 Days to Die";
 
     /**
-     * query_port = client_port - 2
-     * 
-     * ATTENTION: client_port is the game port and NOT base port.
+     * query_port = client_port + 0
      *
      * @type int
      */
-    protected $port_diff = -2;
+    protected $port_diff = 0;
 }
