601b818a22cfcb5fa546909e7de74050d062192a
Nikolay <Rostov114@gmail.com>
2023-10-17 21:26:40 +0400

    Fix code style

--- src/GameQ/Protocols/Rust.php
+++ src/GameQ/Protocols/Rust.php
@@ -48,21 +48,61 @@ class Rust extends Source
      *
      * @type array
      */
-    private $server_keywords = ['mp', 'cp', 'qp', 'born', 'pt', 'h', 'v', 'cs', 'st', 'gm', 'oxide', 'carbon', 'modded'];
+    private $server_keywords = 
+    [
+        'mp',
+        'cp',
+        'qp',
+        'born',
+        'pt',
+        'h',
+        'v',
+        'cs',
+        'st',
+        'gm',
+        'oxide',
+        'carbon',
+        'modded'
+    ];
 
     /**
      * Server tags (https://wiki.facepunch.com/rust/server-browser-tags)
      *
      * @type array
      */
-    private $server_tags = ['monthly', 'biweekly', 'weekly', 'vanilla', 'hardcore', 'softcore', 'pve', 'roleplay', 'creative', 'minigame', 'training', 'battlefield', 'broyale', 'builds'];
+    private $server_tags = 
+    [
+        'monthly',
+        'biweekly',
+        'weekly',
+        'vanilla',
+        'hardcore',
+        'softcore',
+        'pve',
+        'roleplay',
+        'creative',
+        'minigame',
+        'training',
+        'battlefield',
+        'broyale',
+        'builds'
+    ];
 
     /**
      * Region tags (https://wiki.facepunch.com/rust/server-browser-tags)
      *
      * @type array
      */
-    private $region_tags = ['na', 'sa', 'eu', 'wa', 'ea', 'oc', 'af'];
+    private $region_tags = 
+    [
+        'na',
+        'sa',
+        'eu',
+        'wa',
+        'ea',
+        'oc',
+        'af'
+    ];
 
     /**
      * String name of this protocol class
@@ -87,62 +127,51 @@ class Rust extends Source
     {
         $results = parent::processDetails($buffer);
 
-        if (isset($results['keywords']) AND strlen($results['keywords']) > 0)
-        {
+        if (isset($results['keywords']) and strlen($results['keywords']) > 0) {
             $keywords = explode(',', $results['keywords']);
-            if (sizeof($keywords) > 0)
-            {
+            if (sizeof($keywords) > 0) {
                 $results['server.keywords'] = [];
                 $results['unhandled.tags'] = [];
                 $results['server.tags'] = [];
 
-                foreach ($keywords as $gametag)
-                {
-                    $parsed = FALSE;
+                foreach ($keywords as $gametag) {
+                    $parsed = false;
                     $gametag = trim(mb_strtolower($gametag));
 
-                    if (in_array($gametag, $this->server_tags))
-                    {
-                        $parsed = TRUE;
+                    if (in_array($gametag, $this->server_tags)) {
+                        $parsed = true;
 
                         $results['server.tags'][] = $gametag;
                     }
-                    elseif (in_array($gametag, $this->region_tags))
-                    {
-                        $parsed = TRUE;
+                    elseif (in_array($gametag, $this->region_tags)) {
+                        $parsed = true;
 
                         $results['region'] = mb_strtoupper($gametag);
                     }
-                    else
-                    {
+                    else {
                         foreach ($this->server_keywords as $server_keyword)
                         {
                             if (strpos($gametag, $server_keyword) === 0)
                             {
-                                $parsed = TRUE;
+                                $parsed = true;
 
-                                if ($gametag == $server_keyword)
-                                {
-                                    $results['server.keywords'][$gametag] = TRUE;
+                                if ($gametag == $server_keyword) {
+                                    $results['server.keywords'][$gametag] = true;
                                 }
-                                else
-                                {
+                                else {
                                     $results['server.keywords'][$server_keyword] = mb_substr($gametag, mb_strlen($server_keyword));
                                 }
                             }
                         }
                     }
 
-                    if (!$parsed)
-                    {
+                    if (!$parsed) {
                         $results['unhandled.tags'][] = $gametag;
                     }
                 }
 
-                foreach (['cp' => 'num_players', 'mp' => 'max_players'] as $keyword => $key)
-                {
-                    if (isset($results['server.keywords'][$keyword]))
-                    {
+                foreach (['cp' => 'num_players', 'mp' => 'max_players'] as $keyword => $key) {
+                    if (isset($results['server.keywords'][$keyword])) {
                         $results[$key] = intval($results['server.keywords'][$keyword]);
                     }
                 }
